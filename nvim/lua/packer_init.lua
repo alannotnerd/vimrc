28b3aa281ca73352fa46f529fc499276ed1adfa3
alannotnerd <alan1995wang@outlook.com>
2022-08-12 16:27:58 +0800

    Add surround

--- nvim/lua/packer_init.lua
+++ nvim/lua/packer_init.lua
@@ -14,15 +14,15 @@ local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  })
-  vim.o.runtimepath = vim.fn.stdpath("data") .. "/site/pack/*/start/*," .. vim.o.runtimepath
+	packer_bootstrap = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	vim.o.runtimepath = vim.fn.stdpath("data") .. "/site/pack/*/start/*," .. vim.o.runtimepath
 end
 
 -- Autocommand that reloads neovim whenever you save the packer_init.lua file
@@ -36,107 +36,113 @@ vim.cmd([[
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+	return
 end
 
 -- Install plugins
 return packer.startup(function(use)
-  -- Add you plugins here:
-  use("wbthomason/packer.nvim") -- packer can manage itself
+	-- Add you plugins here:
+	use("wbthomason/packer.nvim") -- packer can manage itself
 
-  -- File explorer
-  use("kyazdani42/nvim-tree.lua")
+	-- File explorer
+	use("kyazdani42/nvim-tree.lua")
 
-  -- Indent line
-  use("lukas-reineke/indent-blankline.nvim")
+	-- Indent line
+	use("lukas-reineke/indent-blankline.nvim")
 
-  -- Autopair
-  use({
-    "windwp/nvim-autopairs",
-    config = function()
-      require("nvim-autopairs").setup({})
-    end,
-  })
+	-- Autopair
+	use({
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	})
 
-  -- Icons
-  use("kyazdani42/nvim-web-devicons")
+	-- Icons
+	use("kyazdani42/nvim-web-devicons")
 
-  -- Tag viewer
-  use("preservim/tagbar")
+	-- Tag viewer
+	use("preservim/tagbar")
 
-  -- Treesitter interface
-  use({
-    "nvim-treesitter/nvim-treesitter",
-    run = function()
-      require("nvim-treesitter.install").update({ with_sync = true })
-    end,
-  })
+	-- Treesitter interface
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			require("nvim-treesitter.install").update({ with_sync = true })
+		end,
+	})
 
-  -- Color schemes
-  use("navarasu/onedark.nvim")
-  use("tanvirtin/monokai.nvim")
-  use({ "rose-pine/neovim", as = "rose-pine" })
+	-- Color schemes
+	use("navarasu/onedark.nvim")
+	use("tanvirtin/monokai.nvim")
+	use({ "rose-pine/neovim", as = "rose-pine" })
 
-  -- LSP
-  use("neovim/nvim-lspconfig")
-  use({
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  })
-  use({
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "sumneko_lua", "rust_analyzer" },
-      })
-    end,
-  })
-  use({
-   	"jose-elias-alvarez/null-ls.nvim",
-    config = require("plugins.null-ls"),
-  })
+	-- LSP
+	use("neovim/nvim-lspconfig")
+	use({
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	})
+	use({
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "sumneko_lua", "rust_analyzer" },
+			})
+		end,
+	})
+	use({
+		"jose-elias-alvarez/null-ls.nvim",
+		config = require("plugins.null-ls"),
+	})
 
-  -- Autocomplete
-  use({
-    "hrsh7th/nvim-cmp",
-    requires = {
-      "L3MON4D3/LuaSnip",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-buffer",
-      "saadparwaiz1/cmp_luasnip",
-    },
-    config = function()
-      require("plugins.nvim-cmp")
-    end,
-  })
+	-- Autocomplete
+	use({
+		"hrsh7th/nvim-cmp",
+		requires = {
+			"L3MON4D3/LuaSnip",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-buffer",
+			"saadparwaiz1/cmp_luasnip",
+		},
+		config = function()
+			require("plugins.nvim-cmp")
+		end,
+	})
 
-  -- Statusline
-  use({
-    "feline-nvim/feline.nvim",
-    requires = { "kyazdani42/nvim-web-devicons" },
-  })
+	-- Statusline
+	use({
+		"feline-nvim/feline.nvim",
+		requires = { "kyazdani42/nvim-web-devicons" },
+	})
 
-  -- git labels
-  use({
-    "lewis6991/gitsigns.nvim",
-    requires = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("gitsigns").setup({})
-    end,
-  })
+	-- git labels
+	use({
+		"lewis6991/gitsigns.nvim",
+		requires = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("gitsigns").setup({})
+		end,
+	})
+	use({
+		"kylechui/nvim-surround",
+		config = function()
+			require("nvim-surround").setup()
+		end,
+	})
 
-  -- Dashboard (start screen)
-  use({
-    "goolord/alpha-nvim",
-    requires = { "kyazdani42/nvim-web-devicons" },
-  })
+	-- Dashboard (start screen)
+	use({
+		"goolord/alpha-nvim",
+		requires = { "kyazdani42/nvim-web-devicons" },
+	})
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require("packer").sync()
-  end
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
