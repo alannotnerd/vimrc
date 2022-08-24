88dfc2f58623428106bb63c21af42a3c2602ed1e
alannotnerd <alan1995wang@outlook.com>
2022-08-24 18:17:38 +0800

    Update theme for live_grep

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,21 +2,16 @@ local M = {}
 
 local telescope = require("telescope")
 local builtin = require("telescope.builtin")
-local themes = require('telescope.themes')
-local actions = require('telescope.actions')
 
 function M.setup_keymapping()
   local mappings = {
     ["<space>sf"] = builtin.find_files,
-    ["<space>sg"] = function()
-      builtin.live_grep(themes.get_dropdown())
-    end,
+    ["<space>sg"] = builtin.live_grep,
     ["<space>sl"] = builtin.current_buffer_fuzzy_find,
     ["<space>s*"] = function()
       builtin.grep_string({ grep_open_files = true })
     end,
     ["<space>so"] = builtin.lsp_document_symbols,
-    --builtin.lsp_workspace_symbols,
     ["<space>s"] = builtin.builtin,
     ["<space>sa"] = builtin.diagnostics,
     ["gr"] = builtin.lsp_references,
