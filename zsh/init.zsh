3b279039107e14360518d1c8f0521cc717327907
Alan WANG <alan1995wang@outlook.com>
2023-06-05 16:40:44 +0800

    fix: per history directory

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -19,6 +19,7 @@ autoload -Uz _zinit
 setopt extended_history       # record timestamp of command in HISTFILE
 setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
 setopt hist_ignore_dups       # ignore duplicated commands history list
+setopt inc_append_history
 setopt hist_ignore_space      # ignore commands that start with space
 
 # Load a few important annexes, without Turbo
