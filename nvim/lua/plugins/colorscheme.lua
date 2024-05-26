592a26135f3a9183ebd90837ecf13befd044df99
Alan WANG <alan1995wang@outlook.com>
2024-05-26 21:43:09 +0800

    feat: remove deprecated plugins

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,9 @@
 return {
-  { "loctvl842/monokai-pro.nvim" },
   { "EdenEast/nightfox.nvim" },
-  { "UtkarshVerma/molokai.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "molokai",
+      colorscheme = "catppuccin",
     },
   },
 }
