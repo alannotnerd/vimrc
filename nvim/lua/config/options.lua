81dbebb184ad3d22732b4720686978d9057e31a0
Alan WANG <alan1995wang@outlook.com>
2024-06-12 20:34:22 +0800

    fix: osc52

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,15 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.o.swapfile = false
+-- vim.o.clipboard = "unnamed"
 
--- vim.g.clipboard = {
---   name = "OSC 52",
---   copy = {
---     ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
---     ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
---   },
---   paste = {
---     ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
---     ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
---   },
--- }
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    -- vim.highlight.on_yank()
+    require("vim.ui.clipboard.osc52").copy("+")(vim.v.event.regcontents)
+    require("vim.ui.clipboard.osc52").copy("*")(vim.v.event.regcontents)
+  end,
+})
