58420b8c41904e47cc30cd138e8ad67fb2a6e6de
alannotnerd <alan1995wang@outlook.com>
2025-04-05 16:35:23 +0800

    chore: update blink

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -4,7 +4,7 @@ return {
     completion = {
       menu = {
         draw = {
-          align_to_component = "kind_icon",
+          -- align = "kind_icon",
           padding = { 0, 1 },
           columns = { { "kind_icon" }, { "label", "label_description", gap = 1 }, { "source_name" } },
           components = {
