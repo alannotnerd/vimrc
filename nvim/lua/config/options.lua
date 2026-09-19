1616ce479b70c3b35d361e3560fd89aa25f693fb
Alan WANG <alan1995wang@outlook.com>
2026-09-19 18:49:09 +0800

    chore: allow global osc52

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,12 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.o.swapfile = false
--- vim.o.clipboard = "unnamed"
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    -- vim.highlight.on_yank()
-    require("vim.ui.clipboard.osc52").copy("+")(vim.v.event.regcontents)
-    require("vim.ui.clipboard.osc52").copy("*")(vim.v.event.regcontents)
-  end,
-})
+vim.g.clipboard = "osc52"
